6f749372850ea8feed1e75ab6a8277ccdc5caefd
yacinty <yacinty@users.noreply.github.com>
2018-04-03 16:50:09 +0200

    Repositorio servicio de User

--- src/main/java/forms/UserForm.java
+++ src/main/java/forms/UserForm.java
@@ -28,7 +28,6 @@ public class UserForm {
     private String	phone;
     private String	email;
     private String  postalAddresses;
-    private Date  birthday;
     private boolean	check;
 
     @Column(unique = true)
@@ -101,17 +100,6 @@ public class UserForm {
         this.postalAddresses = postalAddresses;
     }
 
-    @NotNull
-    @Past
-    @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
 
     public boolean getCheck() {
         return this.check;
